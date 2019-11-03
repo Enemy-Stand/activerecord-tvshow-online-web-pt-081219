38326b3307413549418c5198ea54760d4b7803f1
Joseph Sesti <sestijoey63@gmail.com>
2019-11-03 23:43:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,7 +7,7 @@ class Show < ActiveRecord::Base
   
   def self.most_popular_show
     # binding.pry
-    Show.where("rating = ?", highest_rating)
+    self.where("rating = ?", highest_rating)
   end
   
   def self.lowest_rating
