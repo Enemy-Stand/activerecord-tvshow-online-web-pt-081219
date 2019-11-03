2a8ed08947d2f754a427d32de4a3c5db3cac25dc
Joseph Sesti <sestijoey63@gmail.com>
2019-11-03 22:59:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,4 +10,12 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     Show.minimum(:rating)
   end
+  
+  def self.least_popular_show
+    
+  end
+  
+  def self.rating_sum
+    <class Show>.sum(:<rating)
+  end
 end
